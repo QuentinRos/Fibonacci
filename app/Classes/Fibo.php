6bf4968694a7f94f00b2d717ae07ad8662af395b
QuentinRo <quentin.rossier@cpnv.ch>
2020-11-08 20:19:27 +0100

    Ajout du fonctionnement sans valeur maximale

--- app/Classes/Fibo.php
+++ app/Classes/Fibo.php
@@ -2,6 +2,8 @@
 
 namespace App\Classes;
 
+use PhpParser\Node\Expr\Empty_;
+
 class Fibo
 {
     public $nombre;
@@ -15,10 +17,13 @@ class Fibo
             $result = $a + $nombre;
             $a = $nombre;
 
-            // Si la valeur dépasse la valeur maximale demandée :
-            if ($result > $request->max)
-            {
-                break;
+            // Si l'utilisateur n'a pas mis de valeur maximale, itère le nombre de fois demandé
+            if(!empty($request->max)){
+                // Si la valeur dépasse la valeur maximale demandée :
+                if ($result > $request->max)
+                {
+                    break;
+                }
             }
             $nombre = $result;
         }

bbd14d8f722d24365c9feaf46fbd2e07e5d74f8d
QuentinRo <quentin.rossier@cpnv.ch>
2020-11-08 17:58:26 +0100

    Ajout de la gestion du nombre maximum
    S'interromp quand il dépasse la valeur maximum demandé et affiche celle d'avant

--- app/Classes/Fibo.php
+++ app/Classes/Fibo.php
@@ -5,16 +5,23 @@ namespace App\Classes;
 class Fibo
 {
     public $nombre;
+    public $vieuxnombre;
 
-    public function __construct($iteration){
+    public function __construct($request){
         $a = 0;
         $nombre = 1;
-        for($i = 2; $i <=$iteration; $i++)
+        for($i = 2; $i <=$request->iteration; $i++)
         {
             $result = $a + $nombre;
             $a = $nombre;
+
+            if ($result > $request->max)
+            {
+                break;
+            }
             $nombre = $result;
         }
+        $this->vieuxnombre = $a;
         $this->nombre = $nombre;
     }
 

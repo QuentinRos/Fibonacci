3cfc49934f9e9a6530f0473932b9bc05c33a77de
QuentinRo <quentin.rossier@cpnv.ch>
2020-11-08 17:28:43 +0100

    Ajout de l'affichage du résultat de la suite de Fibonacci

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,18 @@
     <div class="content">
         <div class="title m-b-md">
             Fibonacci
-            <form action="/fibonacci" method="post">
+            <form action="/fibonacci" method="get">
+                {{ csrf_field() }}
                 <input type="number" name="iteration" placeholder="Iterations">
+                <input type="number" name="max" placeholder="Nombre maximum">
                 <button type="submit" class="btn btn-primary">
-                    Créer
+                    Calculer
                 </button>
             </form>
+            <div>
+                {{ $fibo->nombre}}
+            </div>
+
         </div>
 
     </div>
